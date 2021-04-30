a15d8b7356459f775de5099ab74e786b8078596d
linhonghao <2407116802@qq.com>
2021-04-30 18:27:51 +0800

    快速排序（未完成）

---
+++ sort/src/com/demo/D01_QuickSort.java
@@ -0,0 +1,37 @@
+package com.demo;
+
+/**
+ * 快速排序
+ *
+ * @author linhonghao@youcheyihou.com
+ * @date 2021/4/30 下午4:45
+ */
+public class D01_QuickSort {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    private void quickSort(int arr[], int left, int right) {
+
+    }
+
+    /**
+     * 调整数组
+     *
+     * @return 返回基准数的索引
+     */
+    private int adjustArr(int arr[], int left, int right) {
+
+        int i = left, j = right;
+        //1.找基准数,默认第一个元素
+        int target = arr[left];
+        //从左到右遍历，把小于的target的值放在左边
+
+
+        return 0;
+    }
+
+
+}
