bbc1ea9cdabd385e62dd34d882eb1c874e46b970
linhonghao <2407116802@qq.com>
2021-05-02 11:29:20 +0800

    快速排序完成

--- sort/src/com/demo/D01_QuickSort.java
+++ sort/src/com/demo/D01_QuickSort.java
@@ -1,5 +1,7 @@
 package com.demo;
 
+import java.util.Arrays;
+
 /**
  * 快速排序
  *
@@ -8,13 +10,23 @@ package com.demo;
  */
 public class D01_QuickSort {
 
-
     public static void main(String[] args) {
-
+        int[] arr = {5, 6, 1, 5, 9, 2, 15, 16};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private void quickSort(int arr[], int left, int right) {
-
+    private static void quickSort(int arr[], int left, int right) {
+        if (arr.length == 0) {
+            return;
+        }
+        if (left < right) {
+            //返回的middle是对应索引已经排好序了
+            int middle = adjustArr(arr, left, right);
+            //采用分治算法
+            quickSort(arr, 0, middle - 1);
+            quickSort(arr, middle + 1, right);
+        }
     }
 
     /**
@@ -22,16 +34,30 @@ public class D01_QuickSort {
      *
      * @return 返回基准数的索引
      */
-    private int adjustArr(int arr[], int left, int right) {
-
+    private static int adjustArr(int arr[], int left, int right) {
         int i = left, j = right;
-        //1.找基准数,默认第一个元素
+        //1.找基准数,默认保存第一个元素，此时相当于在被保存的元素对应的位置挖坑
         int target = arr[left];
-        //从左到右遍历，把小于的target的值放在左边
-
-
-        return 0;
+        while (i < j) {
+            //从右往左遍历，直到找到第一个小于target值的数，填到左边的坑
+            while (i < j && target < arr[j]) {
+                j--;
+            }
+            if (i < j) {
+                arr[i] = arr[j];
+                i++;
+            }
+            //从左往右遍历，直到找到第一个大于或等于target值的数，填到右边的坑
+            while (i < j && target >= arr[i]) {
+                i++;
+            }
+            if (i < j) {
+                arr[j] = arr[i];
+                j--;
+            }
+        }
+        //当i==j时，跳出循环
+        arr[i] = target;
+        return i;
     }
-
-
 }
